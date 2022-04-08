8f57cc848f683b370fa1d54a0a29df035738e494
mustafa badan <mustafabadan1928@gmail.com>
2022-04-09 01:59:39 +0300

    first

---
+++ my-app/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import CustomLink from '@/components/CustomLink';
+
+export default function Home() {
+  return (
+    <>
+     <h1>mohammed nabena </h1>
+    </>
+  );
+}
